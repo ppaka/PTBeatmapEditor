d30a2d6bd7449735a647973f4cbf0f209b165fa1
ppaka <asj0216@gmail.com>
2021-07-20 16:38:11 +0900

    디스코드 GameSDK 적용 및 RPC 작동

---
+++ Assets/Scripts/System/RPC/DiscordController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+public class DiscordController : MonoBehaviour
+{
+    public Discord.Discord discord;
+    public ActivityManager activityManager;
+    public ActivityAssets assets;
+
+    private void Start()
+    {
+        assets.LargeImage = "icontest";
+        discord = new Discord.Discord(865849585778950175, (System.UInt64) CreateFlags.Default);
+        activityManager = discord.GetActivityManager();
+        var activity = new Activity
+        {
+            State = "개발 중",
+            Details = "개발 중",
+            Assets = assets
+        };
+        activityManager.UpdateActivity(activity, (res) =>
+        {
+            if (res == Result.Ok)
+            {
+                Debug.Log("Everything is fine!");
+            }
+        });
+    }
+
+    private void Update()
+    {
+        discord.RunCallbacks();
+    }
+
+    private void OnApplicationQuit()
+    {
+        activityManager.ClearActivity((result => Debug.Log(result)));
+    }
+}
