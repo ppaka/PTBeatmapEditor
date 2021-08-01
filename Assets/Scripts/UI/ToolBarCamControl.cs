7bcfcdad5a1a6e1a9a1be39698f86d01f3877045
ppaka <asj0216@gmail.com>
2021-08-01 22:07:39 +0900

    UI 크기 조정 방식 변경

--- Assets/Scripts/UI/ToolBarCamControl.cs
+++ Assets/Scripts/UI/ToolBarCamControl.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,9 @@ public class ToolBarCamControl : MonoBehaviour
 {
     public Camera mainCam;
     public CanvasScaler scaler;
+    public CanvasGroup canvasGroup;
+    
+    private Coroutine _coroutine;
     private bool _isOn;
 
     private void Update()
@@ -13,18 +17,48 @@ public class ToolBarCamControl : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftControl))
             if (Input.GetKeyDown(KeyCode.T))
             {
-                if (_isOn == false)
-                {
-                    mainCam.DORect(new Rect(0, 0, 1, 0.94f), 0.2f).SetEase(Ease.OutQuint);
-                    //scaler.scaleFactor = 0.94f;
-                    _isOn = true;
-                }
-                else
-                {
-                    mainCam.DORect(new Rect(0, 0, 1, 1), 0.2f).SetEase(Ease.OutQuint);
-                    //scaler.scaleFactor = 1f;
-                    _isOn = false;
-                }
+                if (_coroutine != null)
+                    StopCoroutine(_coroutine);
+                
+                _coroutine = StartCoroutine(ScalingCanvas());
             }
+    }
+
+    private IEnumerator ScalingCanvas()
+    {
+        if (_isOn == false)
+        {
+            mainCam.DOKill();
+            mainCam.DORect(new Rect(0, 0, 1, 0.94f), 0.2f).SetEase(Ease.OutQuint);
+            canvasGroup.DOKill();
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.interactable = false;
+            
+            Tween t = canvasGroup.DOFade(0.6f, 0.4f).SetEase(Ease.Linear);
+            _isOn = true;
+            yield return new DOTweenCYInstruction.WaitForCompletion(t);
+            
+            canvasGroup.DOFade(1f, 0.2f).SetEase(Ease.Linear);
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.interactable = true;
+            scaler.scaleFactor = 0.94f;
+        }
+        else
+        {
+            mainCam.DOKill();
+            mainCam.DORect(new Rect(0, 0, 1, 1), 0.2f).SetEase(Ease.OutQuint);
+            canvasGroup.DOKill();
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.interactable = false;
+            
+            Tween t = canvasGroup.DOFade(0.6f, 0.4f).SetEase(Ease.Linear);
+            _isOn = false;
+            yield return new DOTweenCYInstruction.WaitForCompletion(t);
+            
+            canvasGroup.DOFade(1f, 0.2f).SetEase(Ease.Linear);
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.interactable = true;
+            scaler.scaleFactor = 1;
+        }
     }
 }
