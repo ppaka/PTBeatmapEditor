e89154a6974b6e42afa81a8993f905690eabc7be
ppaka <asj0216@gmail.com>
2021-08-17 16:05:27 +0900

    코드 수정

--- Assets/Scripts/System/SquirrelUpdateManager.cs
+++ Assets/Scripts/System/SquirrelUpdateManager.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Squirrel;
-using Squirrel.SimpleSplat;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 public class SquirrelUpdateManager : MonoBehaviour
@@ -20,75 +14,56 @@ public class SquirrelUpdateManager : MonoBehaviour
     private const string URL = @"https://github.com/PT-GAME/PTBeatmapEditor";
     // https://api.github.com/repos/PT-GAME/PTBeatmapEditor/releases/latest
 
-    readonly ConcurrentQueue<string> _stQueue = new ConcurrentQueue<string>();
-    ConcurrentQueue<float> _flQueue = new ConcurrentQueue<float>();
-    bool isComplete;
-
     async void Start()
     {
         await Task.Run(CheckForUpdate);
     }
 
-    void Update()
-    {
-        if (_stQueue.Count != 0)
-        {
-            _stQueue.TryDequeue(out string uiText);
-            updateText.text = uiText;
-        }
-
-        if (_stQueue.Count != 0)
-        {
-            _flQueue.TryDequeue(out float percent);
-            progImage.fillAmount = percent;
-        }
-
-        if (isComplete)
-        {
-            isComplete = false;
-            button.onClick.AddListener(() => { UpdateManager.RestartApp(); });
-        }
-    }
-
     async Task CheckForUpdate()
     {
-        Squirrel.SimpleSplat.DebugLogger logger = new DebugLogger
+        UnityMainThread.wkr.AddJob(() => { updateText.text = "업데이트 확인 중"; });
+
+        try
         {
-            Level = LogLevel.Debug
-        };
-        logger.Write("Running Task", LogLevel.Debug);
+            using (UpdateManager manager = await UpdateManager.GitHubUpdateManager(URL))
+            {
+                UnityMainThread.wkr.AddJob(() => updateText.text = "업데이트 확인 중");
 
-        using (UpdateManager manager = await UpdateManager.GitHubUpdateManager(URL))
+                if (manager.IsInstalledApp == false)
+                {
+                    UnityMainThread.wkr.AddJob(() => updateText.text = "Squirrel 호환 앱이 아닙니다");
+                    return;
+                }
+
+                UpdateInfo info = await manager.CheckForUpdate();
+
+                if (info.ReleasesToApply.Count == 0)
+                {
+                    UnityMainThread.wkr.AddJob(() => updateText.text = "최신버전 입니다");
+                    return;
+                }
+
+                UnityMainThread.wkr.AddJob(() => progImage.fillAmount = 0);
+                UnityMainThread.wkr.AddJob(() => updateText.text = "업데이트 다운로드중...");
+
+                await manager.DownloadReleases(info.ReleasesToApply,
+                    p => { UnityMainThread.wkr.AddJob(() => progImage.fillAmount = p / 100f); });
+
+                UnityMainThread.wkr.AddJob(() => progImage.fillAmount = 0);
+                UnityMainThread.wkr.AddJob(() => updateText.text = "업데이트 설치중...");
+
+                await manager.ApplyReleases(info, p => progImage.fillAmount = p / 100f);
+
+                UnityMainThread.wkr.AddJob(() => progImage.fillAmount = 0);
+                UnityMainThread.wkr.AddJob(() => updateText.text = "클릭하고 재시작하여 업데이트를 완료해주세요!");
+
+                UnityMainThread.wkr.AddJob(() => button.onClick.AddListener(() => UpdateManager.RestartApp()));
+            }
+        }
+        catch (Exception e)
         {
-            _stQueue.Enqueue("업데이트 확인 중");
-            
-            if (manager.IsInstalledApp == false)
-            {
-                _stQueue.Enqueue("Squirrel 호환 앱이 아닙니다");
-                return;
-            }
-            
-            UpdateInfo info = await manager.CheckForUpdate();
-
-            if (info.ReleasesToApply.Count == 0)
-            {
-                _stQueue.Enqueue("최신버전 입니다");
-                return;
-            }
-
-            _flQueue.Enqueue(0);
-            _stQueue.Enqueue("업데이트 다운로드중...");
-
-            await manager.DownloadReleases(info.ReleasesToApply, p => _flQueue.Enqueue(p / 100f));
-
-            _flQueue.Enqueue(0);
-            _stQueue.Enqueue("업데이트 설치중...");
-
-            await manager.ApplyReleases(info, p => _flQueue.Enqueue(p / 100f));
-
-            _flQueue.Enqueue(0);
-            _stQueue.Enqueue("재시작하여 업데이트를 완료 해주세요!");
-            isComplete = true;
+            UnityMainThread.wkr.AddJob(() => updateText.text = "업데이트를 시도하는 도중 문제가 발생했습니다.");
+            throw new Exception(e.Message);
         }
     }
 }
