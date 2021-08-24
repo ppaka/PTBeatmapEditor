fcc21c73c4bcb6a7b18241998f0d60caa8689946
ppaka <asj0216@gmail.com>
2021-08-25 02:19:42 +0900

    음악 시간 이동에 다른 형식을 추가

--- Assets/Scripts/UI/SongTime.cs
+++ Assets/Scripts/UI/SongTime.cs
@@ -1,86 +1,97 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
 public class SongTime : MonoBehaviour
 {
-	public LevelDataContainer ldc;
-	public SongSlider slider;
-	public AudioSource audioSource;
-	public TMP_InputField songTimeTextField;
+    public LevelDataContainer ldc;
+    public SongSlider slider;
+    public AudioSource audioSource;
+    public TMP_InputField songTimeTextField;
 
-	public bool changed;
+    public bool changed;
 
-	void OnEnable()
-	{
-		SystemEvents.audioLoadComplete += UpdateFirstTime;
-	}
+    void OnEnable()
+    {
+        SystemEvents.audioLoadComplete += UpdateFirstTime;
+    }
 
-	void OnDisable()
-	{
-		SystemEvents.audioLoadComplete -= UpdateFirstTime;
-	}
+    void OnDisable()
+    {
+        SystemEvents.audioLoadComplete -= UpdateFirstTime;
+    }
 
-	void UpdateFirstTime()
-	{
-		DateTime time;
+    void UpdateFirstTime()
+    {
+        DateTime time;
+        
+        bool isPositive = 0 >= (decimal)(ldc.levelData.settings.noteOffset * 0.001f);
 
-		if (0 < ldc.levelData.settings.noteOffset * 0.001f)
-		{
-			time = new DateTime((long) (((decimal)(ldc.levelData.settings.noteOffset * 0.001f) - (decimal) 0) * TimeSpan.TicksPerSecond));
-			songTimeTextField.text = $"-{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
-		}
-		else if (0 >= ldc.levelData.settings.noteOffset * 0.001f)
-		{
-			time = new DateTime((long) (((decimal) 0 - (decimal) (ldc.levelData.settings.noteOffset * 0.001f)) * TimeSpan.TicksPerSecond));
-			songTimeTextField.text = $"{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
-		}
-	}
+        if (!isPositive)
+        {
+            time = new DateTime((long)(((decimal)(ldc.levelData.settings.noteOffset * 0.001f) - 0)
+                                       * TimeSpan.TicksPerSecond));
+            songTimeTextField.text = $"-{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
+        }
+        else
+        {
+            time = new DateTime((long)((0 - (decimal)(ldc.levelData.settings.noteOffset * 0.001f)) *
+                                       TimeSpan.TicksPerSecond));
+            songTimeTextField.text = $"{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
+        }
+    }
 
-	void Update()
-	{
-		if (changed || !audioSource.isPlaying) return;
-		UpdateTime(audioSource.time, false);
-	}
+    void Update()
+    {
+        if (changed || !audioSource.isPlaying) return;
+        UpdateTime(audioSource.time, false);
+    }
 
-	public void MoveTime(string value)
-	{
-		if (int.TryParse(value, NumberStyles.Integer, null, out int time))
-		{
-			time += ldc.levelData.settings.noteOffset;
-			UpdateTime(time * 0.001f, true);
-			slider.slider.value = audioSource.time / audioSource.clip.length;
-		}
-		else
-		{
-			UpdateTime(audioSource.time, false);
-		}
-	}
+    public void MoveTime(string value)
+    {
+        if (songTimeTextField.readOnly) return;
 
-	public void UpdateTime(float songTime, bool changeAudioSourceTime = true)
-	{
-		try
-		{
-			if (changeAudioSourceTime)
-				audioSource.time = songTime;
-			
-			DateTime time;
+        if (int.TryParse(value, NumberStyles.Integer, null, out int time))
+        {
+            time += ldc.levelData.settings.noteOffset;
+            UpdateTime(time * 0.001f);
+            slider.slider.value = audioSource.time / audioSource.clip.length;
+        }
+        else if (SongTimeConverter.ToInt(value, out time))
+        {
+            time += ldc.levelData.settings.noteOffset;
+            UpdateTime(time * 0.001f);
+            slider.slider.value = audioSource.time / audioSource.clip.length;
+        }
+        else
+        {
+            UpdateTime(audioSource.time, false);
+        }
+    }
 
-			if (songTime < ldc.levelData.settings.noteOffset * 0.001f)
-			{
-				time = new DateTime((long) (((decimal)(ldc.levelData.settings.noteOffset * 0.001f) - (decimal) songTime) * TimeSpan.TicksPerSecond));
-				songTimeTextField.text = $"-{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
-			}
-			else if (songTime >= ldc.levelData.settings.noteOffset * 0.001f)
-			{
-				time = new DateTime((long) (((decimal) songTime - (decimal) (ldc.levelData.settings.noteOffset * 0.001f)) * TimeSpan.TicksPerSecond));
-				songTimeTextField.text = $"{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
-			}
-		}
-		catch
-		{
-			//
-		}
-	}
+    public void UpdateTime(float songTime, bool changeAudioSourceTime = true)
+    {
+        if (changeAudioSourceTime)
+            audioSource.time = songTime;
+
+        DateTime time;
+
+        bool isMinus = 0 > (decimal)songTime - (decimal)(ldc.levelData.settings.noteOffset * 0.001f);
+
+        if (isMinus)
+        {
+            time = new DateTime((long)(((decimal)(ldc.levelData.settings.noteOffset * 0.001f) - (decimal)songTime)
+                                       * TimeSpan.TicksPerSecond));
+            songTimeTextField.text = $"-{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
+        }
+        else
+        {
+            time = new DateTime((long)(((decimal)songTime - (decimal)(ldc.levelData.settings.noteOffset * 0.001f)) *
+                                       TimeSpan.TicksPerSecond));
+            songTimeTextField.text = $"{time.Hour:00}:{time.Minute:00}:{time.Second:00};{time.Millisecond:000}";
+        }
+    }
 }
---
+++ Assets/Scripts/Utils/SongTimeConverter.cs
@@ -0,0 +1,133 @@
+﻿using System.Text.RegularExpressions;
+
+public static class SongTimeConverter
+{
+    public static bool ToInt(string str, out int result)
+    {
+        int value = 0;
+        
+        if (str.Contains(";"))
+        {
+            if (str.Split(';').Length > 2){
+                result = 0;
+                return false;
+            }
+            string[] split1 = str.Split(':');
+
+            var sec = split1[split1.Length-1].Split(';')[0];
+            var msec = split1[split1.Length - 1].Split(';')[1];
+
+            split1[split1.Length - 1] = sec;
+
+            if (split1.Length == 3)
+            {
+                for (int i = 0; i < split1.Length; i++)
+                {
+                    if (split1[i].Length < 3) continue;
+                    result = 0;
+                    return false;
+                }
+                
+                if (msec.Length > 3)
+                {
+                    result = 0;
+                    return false;
+                }
+
+                if (msec.Length == -1)
+                {
+                    msec = "0";
+                }
+
+                value += int.Parse(split1[0]) * 3600 * 1000;
+                value += int.Parse(split1[1]) * 60 * 1000;
+                value += int.Parse(split1[2]) * 1000;
+                value += int.Parse(msec);
+            }
+            else if (split1.Length == 2)
+            {
+                if (msec.Length > 3)
+                {
+                    result = 0;
+                    return false;
+                }
+                if (msec.Length == -1)
+                {
+                    msec = "0";
+                }
+                
+                value += int.Parse(split1[0]) * 60 * 1000;
+                value += int.Parse(split1[1]) * 1000;
+                value += int.Parse(msec);
+            }
+            else if (split1.Length == 1)
+            {
+                if (msec.Length > 3)
+                {
+                    result = 0;
+                    return false;
+                }
+                if (msec.Length == -1)
+                {
+                    msec = "0";
+                }
+                
+                value += int.Parse(split1[0]) * 1000;
+                value += int.Parse(msec);
+            }
+            else if (split1.Length > 3)
+            {
+                result = 0;
+                return false;
+            }
+        }
+        else
+        {
+            var split1 = str.Split(':');
+
+            if (split1.Length == 3)
+            {
+                for (int i = 0; i < split1.Length; i++)
+                {
+                    if (split1[i].Length < 3) continue;
+                    result = 0;
+                    return false;
+                }
+
+                value += int.Parse(split1[0]) * 3600 * 1000;
+                value += int.Parse(split1[1]) * 60 * 1000;
+                value += int.Parse(split1[2]) * 1000;
+            }
+            else if (split1.Length == 2)
+            {
+                for (int i = 0; i < split1.Length; i++)
+                {
+                    if (split1[i].Length < 3) continue;
+                    result = 0;
+                    return false;
+                }
+                
+                value += int.Parse(split1[0]) * 60 * 1000;
+                value += int.Parse(split1[1]) * 1000;
+            }
+            else if (split1.Length == 1)
+            {
+                for (int i = 0; i < split1.Length; i++)
+                {
+                    if (split1[i].Length < 3) continue;
+                    result = 0;
+                    return false;
+                }
+                
+                value += int.Parse(split1[0]) * 1000;
+            }
+            else if (split1.Length > 3)
+            {
+                result = 0;
+                return false;
+            }
+        }
+        result = value;
+        return true;
+    }
+}
